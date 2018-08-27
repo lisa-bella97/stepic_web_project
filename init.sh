68e76c374dafa7da80613cf534b9eb55e122e22b
Elizaveta <lisa-bella97@mail.ru>
2018-08-27 23:34:44 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
