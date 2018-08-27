50160757f5d4fc4d826fd5c31bc01df354bb2ef1
Elizaveta <lisa-bella97@mail.ru>
2018-08-27 17:50:47 +0300

    Added hello.py

---
+++ hello.py
@@ -0,0 +1,5 @@
+def wsgi_application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    start_response(status, headers)
+    return environ['QUERY_STRING'].split('&')
